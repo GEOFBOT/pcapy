5cd293edfac7228e2eaa5f72be2c3fe74bdb335d
Geoffrey Mon <geofbot@gmail.com>
2016-01-13 20:39:31 -0500

    Hacky, basic support for nanosecond timestamps for live packet capture

--- pcapy.cc
+++ pcapy.cc
@@ -79,11 +79,12 @@ open_live(PyObject *self, PyObject *args)
   int  snaplen;
   int  promisc;
   int  to_ms;
+  int nano;
   
   bpf_u_int32 net, mask;
   
   
-  if(!PyArg_ParseTuple(args,"siii:open_live",&device,&snaplen,&promisc,&to_ms))
+  if(!PyArg_ParseTuple(args,"siii|i:open_live",&device,&snaplen,&promisc,&to_ms,&nano))
     return NULL;
   
   int status = pcap_lookupnet(device, &net, &mask, errbuff);
@@ -95,7 +96,12 @@ open_live(PyObject *self, PyObject *args)
   
   pcap_t* pt;
 	
-  pt = pcap_open_live(device, snaplen, promisc!=0, to_ms, errbuff);
+  pt = pcap_create(device, errbuff);
+  pcap_set_snaplen(pt, snaplen);
+  pcap_set_promisc(pt, promisc!=0);
+  pcap_set_timeout(pt, to_ms);
+  pcap_set_tstamp_precision(pt, nano == 1 ? PCAP_TSTAMP_PRECISION_NANO : PCAP_TSTAMP_PRECISION_MICRO );
+  pcap_activate( pt );
   if(!pt)
     {
       PyErr_SetString(PcapError, errbuff);
